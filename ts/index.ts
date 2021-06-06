82b8358d180646235886322a6a98da600844d973
SS-13 <samfec@163.com>
2021-06-06 11:13:22 +0800

    .,

--- ts/index.ts
+++ ts/index.ts
@@ -9,3 +9,21 @@ function tuplify<T extends unknown[]>(...elements: T): T {
 
 // const temp: [string, number]
 const temp = tuplify("Sam", 32);
+
+/**
+ * 强制类型校验
+ * @param obj
+ * @param key
+ * @returns
+ */
+function mandatoryTypeCheck<T extends object, K extends keyof T>(
+  obj: T,
+  key: K
+): T[K] {
+  return obj[key];
+}
+const obj = {
+  a: 1,
+  hello: "world",
+};
+console.log(mandatoryTypeCheck(obj, "a"));
